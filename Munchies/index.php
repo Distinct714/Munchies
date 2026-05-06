d28e32eea65975662e28339c8f86fda2b845d41b
Distinct <distinct3579@gmail.com>
2026-05-06 09:07:13 +0800

    Update index.php

--- Munchies/index.php
+++ Munchies/index.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Munchies | Gourmet Muffins</title>
+        <title>Munchies | Affordable Muffins</title>
         <link rel="stylesheet" href="static/style.css">
 
     </head>
