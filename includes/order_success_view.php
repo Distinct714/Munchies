a3ead165786034c39658f5f9bdc2249b7507c191
Distinct <distinct3579@gmail.com>
2026-06-17 13:22:40 +0800

    Make some adjustment, nothing special.

--- includes/order_success_view.php
+++ includes/order_success_view.php
@@ -13,7 +13,7 @@ unset($_SESSION['order_success']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Success | Munchies</title>
-    <link rel="stylesheet" href="static/style3.css">
+    <link rel="stylesheet" href="static/style4.css">
 
     <style>
         .success-shell {
